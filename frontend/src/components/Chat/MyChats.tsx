8f463063e7a29d4499ae13f9cbfcf319a7d34d21
bahaa-alden-abdo <samerabood195@gmail.com>
2023-04-24 15:38:32 +0300

    real

--- frontend/src/components/Chat/MyChats.tsx
+++ frontend/src/components/Chat/MyChats.tsx
@@ -16,7 +16,7 @@ import { getSender } from '../../config/chatLogics';
 import GroupChatModel from '../miscellaneous/GroupChatModel';
 import { Chat } from '../../types/interfaces';
 import io, { Socket } from 'socket.io-client';
-const ENDPOINT = 'https://biogram.onrender.com';
+const ENDPOINT = 'https://biogram.onrender.com/';
 let socket: Socket;
 
 function MyChat({ fetchAgain, bg }: any) {
