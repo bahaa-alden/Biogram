1e109891a25cc1a5c4eaa09c7989cf12145ed457
bahaa-alden-abdo <samerabood195@gmail.com>
2023-04-27 21:07:41 +0300

    change notification type

--- backend/types/notification.type.ts
+++ backend/types/notification.type.ts
@@ -1,9 +1,12 @@
 import { Document, Model, Schema } from 'mongoose';
+import { IUser } from './user.type';
+import { IMessage } from './message.type';
+import { IChat } from './chat.type';
 
 export interface INotification {
-  user: Schema.Types.ObjectId;
-  message: Schema.Types.ObjectId;
-  chat: Schema.Types.ObjectId;
+  user: Schema.Types.ObjectId | IUser;
+  message: Schema.Types.ObjectId | IMessage;
+  chat: Schema.Types.ObjectId | IChat;
   read: boolean;
 }
 
